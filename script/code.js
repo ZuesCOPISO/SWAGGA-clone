60df8c1189a0ee2cc5d212f9c0e95095767e8f3a
ZeusCOPISO <copisozizipho2@gmail.com>
2024-05-31 15:01:32 +0200

    updated css

--- script/code.js
+++ script/code.js
@@ -1,14 +1,18 @@
-document.querySelector('[confirm]').addEventListener('click',calculate)
-function calculate () {
+document.querySelector('[confirm]').addEventListener('click', calculate)
+function calculate() {
     let femalequantity = +document.querySelector('[femaleqty]').value
     let malequantity = +document.querySelector('[maleqty]').value
-    let Output1= document.querySelector('[femaleOutput]')
-    let Output2= document.querySelector('[maleOutput]')
-    let price1= +document.querySelector('#femalePrice')
-    let price2= +document.querySelector('#femalePrice')
+    let Output1 = document.querySelector('[femaleOutput]')
+    let Output2 = document.querySelector('[maleOutput]')
+    let price1 = +document.querySelector('[femalePrice]').textContent
+    let price2 = +document.querySelector('[malePrice]').textContent
     let totalPrice = document.querySelector('#totalBtn')
-    Output1.innerText = parseFloat(`${price1.innerText}*${femalequantity}`).toFixed(2)
-Output2.innerText = parseFloat(`${price2.innerText}*${malequantity}`).toFixed(2)
-totalPrice.innerText = parseFloat(`${Output1.innerText}+${Output2.innerText}`).toFixed(2)
+    Output1.innerText = parseFloat(eval(`${price1}*${femalequantity}`)).toFixed(2)
+    Output2.innerText = parseFloat(eval(`${price2}*${malequantity}`)).toFixed(2)
+    totalPrice.innerText = parseFloat(eval(`${Output1.innerText}+${Output2.innerText}`)).toFixed(2)
 }
+
+
+
+
   
